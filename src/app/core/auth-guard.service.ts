5bece5a169a42ee97e595a7326df19de7275808f
John Papa <john@johnpapa.net>
2016-09-28 01:28:38 -0400

    added toaster to auth guard

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -9,18 +9,27 @@ import {
   RouterStateSnapshot
 } from '@angular/router';
 
+import { ToastService } from './toast/toast.service';
 import { UserProfileService } from './user-profile.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
-  constructor(private userProfileService: UserProfileService, private router: Router) { }
+  deniedMessage = 'Unauthorized access denied';
+
+  constructor(
+    private userProfileService: UserProfileService,
+    private toastService: ToastService,
+    private router: Router) { }
 
   canLoad(route: Route) {
     if (this.userProfileService.isLoggedIn) {
       return true;
     }
+
+    var message = 'Unauthorized access denied';
     let url = `/${route.path}`;
     this.router.navigate(['/login'], { queryParams: { redirectTo: url } });
+    this.toastService.activate(this.deniedMessage);
     return this.userProfileService.isLoggedIn;
   }
 
@@ -32,7 +41,7 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
       return true;
     }
     this.router.navigate(['/login'], { queryParams: { redirectTo: state.url } });
-
+    this.toastService.activate(this.deniedMessage);
     return false;
   }
 
