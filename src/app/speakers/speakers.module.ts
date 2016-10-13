f75f10a7a8a4202a377388327fe88e94daedf6ca
John Papa <john@johnpapa.net>
2016-10-12 20:28:58 -0400

    renamed modules for routing

--- src/app/speakers/speakers.module.ts
+++ src/app/speakers/speakers.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SpeakerButtonComponent } from './shared/speaker-button/speaker-button.component';
 import { SortSpeakersPipe } from './shared/sort-speakers.pipe';
-import { SpeakersRoutingModule, routedComponents } from './speakers.routing';
+import { SpeakersRoutingModule, routedComponents } from './speakers-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
